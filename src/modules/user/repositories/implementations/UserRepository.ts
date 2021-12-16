386631e274617ce7d6a9042cb8eb0e7ea795ae34
Gabriel Alves <gabe.alvesch@gmail.com>
2021-12-16 10:13:23 -0300

    add: prisma user repository

---
+++ src/modules/user/repositories/implementations/UserRepository.ts
@@ -0,0 +1,93 @@
+import { hash } from "bcrypt";
+
+import { PrismaClient } from "@prisma/client";
+
+import { IUpdatedUserData } from "../../DTOs/IUpdatedUserData";
+import { IUserData } from "../../DTOs/IUserData";
+import { User } from "../../entities/User";
+import { IUserRepository } from "../IUserRepository";
+
+class UserRepository implements IUserRepository {
+  private client: PrismaClient;
+
+  constructor() {
+    this.client = new PrismaClient();
+  }
+
+  async list(page: number): Promise<User[]> {
+    const users = await this.client.users.findMany({
+      skip: 7 * page,
+      take: 7,
+    });
+
+    return users;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.client.users.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.client.users.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  }
+
+  async save(data: IUserData): Promise<User> {
+    const user = new User({
+      name: data.name,
+      email: data.email,
+      password: await hash(data.password, 8),
+    });
+
+    await this.client.users.create({
+      data: {
+        ...user,
+      },
+    });
+
+    return user;
+  }
+
+  async update(data: IUpdatedUserData): Promise<User> {
+    const updatedUser = new User(
+      {
+        name: data.name,
+        email: data.email,
+        password: await hash(data.password, 8),
+      },
+      data.id
+    );
+
+    await this.client.users.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        ...updatedUser,
+      },
+    });
+
+    return updatedUser;
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.client.users.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
+
+export { UserRepository };
