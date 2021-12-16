e3f3ec658e2695b149e24968182c1714218635ae
Gabriel Alves <gabe.alvesch@gmail.com>
2021-12-16 10:36:04 -0300

    fix: pagination

--- src/modules/user/useCases/listUsers/ListUsersController.ts
+++ src/modules/user/useCases/listUsers/ListUsersController.ts
@@ -5,10 +5,12 @@ import { ListUsersUseCase } from "./ListUsersUseCase";
 
 class ListUsersController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const page = parseInt(request.params.page, 10) || 0;
+    const { page } = request.query;
+
+    const pagination = parseInt(page as string, 10) || 0;
 
     const listUsersUseCase = container.resolve(ListUsersUseCase);
-    const users = await listUsersUseCase.execute({ page });
+    const users = await listUsersUseCase.execute({ page: pagination });
 
     return response.status(200).json(users);
   }
